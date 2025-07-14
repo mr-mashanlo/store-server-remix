5883551bd70e6b05c44dcf848d1e8fc6e877d83e
mr-mashanlo <mr.mashanlo@yandex.ru>
2025-07-14 13:23:22 +0500

    add user to express

---
+++ src/express.d.ts
@@ -0,0 +1,5 @@
+declare namespace Express {
+  export interface Request {
+    user?: { id: string, iat: number, exp: number }
+  }
+}
--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,11 +1,13 @@
 import { NextFunction, Request, Response } from 'express';
 
 import { CustomError } from '@/utils/error';
+import { decodeToken } from '@/utils/token';
 
 export const authMiddleware = async ( req: Request, res: Response, next: NextFunction ) => {
   try {
     const token = req.cookies.token;
     if ( !token ) throw new CustomError( { status: 419, name: 'token', message: 'Token not found' } );
+    req.user = decodeToken( token );
     next();
   } catch ( error ) {
     next( error );
